edb283df65dfd76c3b1af271ed1ee83468a5dccc
pedro <troopper.san@gmail.com>
2025-01-28 11:26:43 -0300

    Primeiro site, com alguns bugs a resolver

--- js/exercicio.js
+++ js/exercicio.js
@@ -1,1 +1,16 @@
-alert("Oi")
+
+
+function carga(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('foto')  
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = 'Agora são ' + hora + ' horas'
+    if(hora >=0 && hora<12){
+       img.src='../images/Manhã.png'
+    }else if(hora>=12 && hora<=18){
+        img.src='../images/Tarde.png'
+    }else{
+        img.src='../images/Noite.png'
+    }
+}
