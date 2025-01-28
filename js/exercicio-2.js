2ec8b66837daf04ac2010dfaf3d52d1fceae1927
pedro <troopper.san@gmail.com>
2025-01-28 12:36:17 -0300

    Resolvendo bug, e verificando a idade corretamente

--- js/exercicio-2.js
+++ js/exercicio-2.js
@@ -1,20 +1,15 @@
 function verificador_de_idade(){
-    var img = document.getElementById('masc');
     var inputAno = document.getElementById('idade').value ;
     var sexo = document.querySelector('input[name="rad_sexo"]:checked').value;
+    console.log(sexo)
     var data = new Date();
     var atual = data.getFullYear();
     var idade = atual - inputAno;
-    var res = document.getElementById('res')
-    if (inputAno == 0 || inputAno > atual) {
-        resultado.innerHTML = '[ERRO] Verifique os dados e tente novamente!';
+    var res = document.getElementById('resultado')
+    if (inputAno == 0 || inputAno > atual || !sexo) { // Adicionada verificação para 'sexo'
+        res.innerHTML = '[ERRO] Verifique os dados e tente novamente!';
     } else {
-        var genero = '';
-        if (sexo == 'masculino') {
-            genero = 'Homem';
-        } else if (sexo == 'feminino') {
-            genero = 'Mulher';
-        }
-        resultado.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+        var genero = sexo.value == 'masculino' ? 'Homem' : 'Mulher'; // Simplificado aqui
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
     }
 }
