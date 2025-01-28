92426ef624e01b7cc23f1c72dc0d7ab9af5dc0b4
pedro <troopper.san@gmail.com>
2025-01-28 12:45:18 -0300

    Correção final

--- js/exercicio-2.js
+++ js/exercicio-2.js
@@ -1,15 +1,15 @@
 function verificador_de_idade(){
     var inputAno = document.getElementById('idade').value ;
     var sexo = document.querySelector('input[name="rad_sexo"]:checked').value;
-    console.log(sexo)
     var data = new Date();
     var atual = data.getFullYear();
     var idade = atual - inputAno;
     var res = document.getElementById('resultado')
-    if (inputAno == 0 || inputAno > atual || !sexo) { // Adicionada verificação para 'sexo'
+    if (inputAno == 0 || inputAno > atual) { // Adicionada verificação para 'sexo'
         res.innerHTML = '[ERRO] Verifique os dados e tente novamente!';
     } else {
-        var genero = sexo.value == 'masculino' ? 'Homem' : 'Mulher'; // Simplificado aqui
+        var genero = (sexo === 'masculino') ? 'Homem' : 'Mulher'; // Corrigido aqui
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+        
     }
 }
